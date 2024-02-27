28810bd1976f28b1fce3376944ee84c976ee0e39
Mark <toesing.mark@gmail.com>
2024-02-27 08:16:12 +0100

    Made Singup and login Imported the libaries to help with that Some variable changes

--- Main.py
+++ Main.py
@@ -1,15 +1,25 @@
 from phrases import *
 from random import randint
 import time
+import datetime
+import os
+
+now = datetime.datetime.now()
+CurrentDateTime = now.strftime("%d/%m/%Y %H:%M:%S")
+
+
+CurrentDirectory = os.getcwd()
+UsersDirectory = f"{CurrentDirectory}/Users"
+OSUserDirectory = os.path.join(UsersDirectory)
 
 PhraseType = phrase[randint(0,len(phrase)-1)]
-LetterPhrase = []
-LetterUser = []
+LetterPhraseSplitted = []
+UserPhrase = []
 def PhraseSplit(SplittingPhrase, List):
     for Letter in SplittingPhrase:
         List.append(Letter)
 
-print(LetterPhrase)
+# print(LetterPhraseSplitted)
 
 def StartTime():
     time.time()
@@ -17,15 +27,38 @@ def StartTime():
 def EndTime():
     time.time()
 
+os.mkdir
+
+def SignUp():
+    UserName = input("Please enter your Username: ")
+    UserDir = os.path.join(OSUserDirectory, UserName)
+    try:
+        os.mkdir(UserDir)
+    except:
+        print("That User Already Exists try again \n")
+        SignUp()
+
+def Login():
+    CurrentUser = input("Please enter the username: ")
+    UserDir = os.path.join(OSUserDirectory, CurrentUser)
+    try:
+        os.mkdir(UserDir)
+    except: # If successful login
+        input("Test")
+    else:
+        print("User doesn't exist... Try again\n")
+        os.rmdir(UserDir)
+        Login()
 
 
 def main():
     print("Please enter this phrase as fast as you can:\n")
-    PhraseSplit(PhraseType, LetterPhrase)
+    PhraseSplit(PhraseType, LetterPhraseSplitted)
     time.sleep(1)
     print(PhraseType)
     StartTime()
     UserInput = input()
     EndTime()
-    PhraseSplit(UserInput, LetterUser)
+    PhraseSplit(UserInput, UserPhrase)
 
+Login()
