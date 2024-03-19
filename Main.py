dba47d6ced91713d7455a4619c2b0009fa859d32
Mark <toesing.mark@gmail.com>
2024-03-19 10:26:30 +0100

    Bunch of code

--- Main.py
+++ Main.py
@@ -7,7 +7,8 @@ import os
 
 # Variables
 now = datetime.datetime.now()
-CurrentDateTime = now.strftime("%d/%m/%Y %H:%M:%S")
+CurrentDate = str(now.strftime("%d-%m-%Y"))
+CurrentTime = str(now.strftime("%H.%M"))
 
 CurrentDirectory = os.getcwd()
 CreateUserDir = os.path.join(CurrentDirectory, "Users")
@@ -15,53 +16,27 @@ UsersDirectory = f"{CurrentDirectory}/Users"
 OSUserDirectory = os.path.join(UsersDirectory)
 
 PhraseType = phrase[randint(0,len(phrase)-1)]
-LetterPhraseSplitted = []
-UserPhrase = []
+MainPhraseSplitted = []
+UserPhraseSplitted = []
 
 # Functions
 def PhraseSplit(SplittingPhrase, List):
-    """
-    The function `PhraseSplit` takes a phrase and splits it into individual letters, adding each letter
-    to a list.
-    
-    :param SplittingPhrase: The `SplittingPhrase` parameter is the phrase or string that you want to
-    split into individual letters
-    :param List: The `List` parameter in the `PhraseSplit` function is a list that will store the
-    individual letters of the `SplittingPhrase` after splitting
-    """
     for Letter in SplittingPhrase:
         List.append(Letter)
 
 def TimerStart():
-    """
-    The function `TimerStart()` returns the current time when called.
-    :return: The function `TimerStart()` is returning the current time when it is called using
-    `time.time()`.
-    """
     return time.time()
 
 def TimerEnd():
-    """
-    The function `TimerEnd` returns the current time when called.
-    :return: The function `TimerEnd()` is returning the current time when it is called using
-    `time.time()`.
-    """
     return time.time()
 
 def MakeStartUserDir():
-    """
-    The function attempts to create a Users folder if it does not already exist.
-    """
     try: # Make the Users Folder if it doesn't exist
         os.mkdir(CreateUserDir)
     except:
         pass
 
 def SignUp():
-    """
-    The function `SignUp` prompts the user to enter a username, creates a directory with that username,
-    and handles cases where the username already exists.
-    """
     UserName = input("Please enter your Username: ")
     UserDir = os.path.join(OSUserDirectory, UserName)
     try:
@@ -71,10 +46,7 @@ def SignUp():
         SignUp()
 
 def Login():
-    """
-    The `Login` function prompts the user to enter a username, creates a directory based on the
-    username, and handles login success or failure.
-    """
+    global CurrentUser
     CurrentUser = input("Please enter the username: ")
     UserDir = os.path.join(OSUserDirectory, CurrentUser)
     try:
@@ -88,10 +60,6 @@ def Login():
 
 
 def LoginSignupChoise():
-    """
-    The function `LoginSignupChoise` prompts the user to choose between logging in or signing up and
-    calls the respective functions based on the input.
-    """
     Login_Signup = input("Do you want to log in (L) or sign up (S)?\n")
 
     if Login_Signup.lower() == "l":
@@ -104,35 +72,59 @@ def LoginSignupChoise():
 
 
 def SetTime(StartTime, EndTime):
-    """
-    The function SetTime calculates the difference in time between a start time and an end time.
+    int(EndTime - StartTime)
+
+
+def AddTime(time, accuracy = 0):
+    Directory = f"{os.getcwd()}/Users/{CurrentUser}"
+    Place = os.path.join(Directory, f"{CurrentDate} {CurrentTime}.txt")
+    with open(Place, "w") as File:
+        File.write(f"Date: {CurrentDate}\n")
+        File.write(f"Clock Time: {CurrentTime}\n")
+        File.write(f"Your Time: {time} secs \n")
+        File.write(f"Your accuracy: {accuracy}%\n")
+
+def Checker(Phrase1, Phrase2):
+    global CorrectCounter
+    CorrectCounter = 0
+    for index in range(len(Phrase1)):
+        if Phrase1[index] == Phrase2[index]:
+            CorrectCounter += 1
+
+
+        
+def Accuracy():
+    if len(MainPhraseSplitted) >= len(UserPhraseSplitted):
+        Checker(UserPhraseSplitted, MainPhraseSplitted)
+    else:
+        Checker(MainPhraseSplitted,UserPhraseSplitted)
     
-    :param StartTime: The `StartTime` parameter represents the starting time of an event or process. It
-    could be a specific point in time, such as the beginning of a task or an event
-    :param EndTime: The `EndTime` parameter represents the time when a particular event or process ends
-    :return: The function SetTime returns the difference between the EndTime and StartTime as an
-    integer.
-    """
-    return int(EndTime - StartTime)
+    global AccuracyPercent
+    if CorrectCounter != 0:
+        AccuracyPercent = int(CorrectCounter/len(MainPhraseSplitted)*100)
+    else:
+        AccuracyPercent = 0
 
-
-def main():
+def Main():
     MakeStartUserDir()
     LoginSignupChoise()
 
     print("Please enter this phrase as fast as you can:\n")
-    PhraseSplit(PhraseType, LetterPhraseSplitted)
+    PhraseSplit(PhraseType, MainPhraseSplitted)
     print(PhraseType,"\n")
 
     StartTime = TimerStart()
     UserInput = input()
     EndTime = TimerEnd()
-    print(f"Your Time was: {SetTime(StartTime, EndTime)} secs")
+    Time = SetTime(StartTime, EndTime)
 
-    PhraseSplit(UserInput, UserPhrase)
+    PhraseSplit(UserInput, UserPhraseSplitted)
+    Accuracy()
+
+    AddTime(Time, AccuracyPercent)
 
 
 
 
 if __name__ == "__main__":
-    main()
+    Main()
